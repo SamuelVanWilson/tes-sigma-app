bfc0b51f094db80f91a9d88ac3ee04f9305eb03e
SamuelVan <samuelvanmartino@gmail.com>
2024-07-21 10:26:19 +0700

    menyesuaikan tampilan di hp

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const poinBars = document.querySelectorAll('.poinBar');
     const formUser = document.querySelector('form')
     const jenisKepribadian = document.getElementById('persona')
+    let gambarPersona = document.querySelector('section>img')
     const originalValuesPoin = [];
     let kepriBadianAsli;
     let nama;
@@ -12,13 +13,18 @@ document.addEventListener('DOMContentLoaded', function(){
       e.preventDefault()
       const formData = new FormData(formUser);
       nama = formData.get('nama').toLowerCase()
-      console.log(nama);
-      viewPoinSigma.style.setProperty('display', 'block')
-      formUser.style.setProperty('display', 'none')
-      document.querySelector('#viewSigma>h1').innerHTML = nama
+      document.querySelector('#viewSigma>section>h1').innerHTML = nama
       let userData = JSON.parse(localStorage.getItem('userData')) || {};
-
+      if (nama === '' || nama.length <= 2) {
+        e.preventDefault()
+        alert('nama harus di isi dan lebih 2 huruf')
+      } else {
+      viewPoinSigma.style.setProperty('display', 'block')
+      setTimeout(() => {
+        document.querySelector('main').style.setProperty('transform', 'rotateY(-180deg)')
+      }, 300);
         setTimeout(() => {
+          formUser.style.setProperty('display', 'none')
           if (userData[nama]) {
             console.log(userData[nama])
             userData[nama].forEach(poinUser => {
@@ -28,8 +34,8 @@ document.addEventListener('DOMContentLoaded', function(){
               let value = originalValuesPoin[index]
               poinBar.style.width = value + '%';
               setTimeout(() => {
-                poinBar.innerHTML = value;
-              }, 200);
+                poinBar.innerHTML = value + '%';
+              }, 520);
             });
           } else {
             poinBars.forEach(function(poinBar, index) {
@@ -39,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function(){
               poinBar.style.width = randomValue + '%';
               setTimeout(() => {
                 poinBar.innerHTML = randomValue + '%';
-              }, 200);
+              }, 520);
             });
             console.log(userData);
             userData[nama] = originalValuesPoin;
@@ -48,34 +54,40 @@ document.addEventListener('DOMContentLoaded', function(){
           jenisKepribadian.textContent = randomPersona()
         }, 400);
 
-      if(!intervalId) {
-        intervalId = setInterval(function() {
-          console.log('woi');
-          if (jenisKepribadian.innerText != kepriBadianAsli) {
-           jenisKepribadian.innerText = kepriBadianAsli 
-          }
-          poinBars.forEach(function(poinBar, index) {
-            const originalValue = originalValuesPoin[index];
-            if (parseInt(poinBar.style.width) !== originalValue) {
-              poinBar.style.width = originalValue + '%';
-              poinBar.textContent = originalValue + '%';
+        if(!intervalId) {
+          intervalId = setInterval(function() {
+            console.log('woi');
+            if (jenisKepribadian.innerText != kepriBadianAsli) {
+            jenisKepribadian.innerText = kepriBadianAsli 
             }
-          });
-        }, 1000);
-    }
+            poinBars.forEach(function(poinBar, index) {
+              const originalValue = originalValuesPoin[index];
+              if (parseInt(poinBar.style.width) !== originalValue) {
+                poinBar.style.width = originalValue + '%';
+                poinBar.textContent = originalValue + '%';
+              }
+            });
+          }, 1000);
+        }
+      }
     })
 
 
 
     document.querySelector('button[type="button"]').addEventListener('click', function () {
       clearInterval(intervalId)
-      originalValuesPoin.length = 0;
-      poinBars.forEach(poinBar => {
-        poinBar.style.width = 0 + '%' 
-        poinBar.innerHTML = ''
-      });
-      viewPoinSigma.style.setProperty('display', 'none');
-      formUser.style.setProperty('display', 'block')
+      formUser.style.setProperty('display', 'flex')
+      setTimeout(() => {
+        document.querySelector('main').style.setProperty('transform', 'rotateY(0deg)')
+        setTimeout(() => {
+          originalValuesPoin.length = 0;
+          poinBars.forEach(poinBar => {
+            poinBar.style.width = 0 + '%' 
+            poinBar.innerHTML = ''
+          });
+          viewPoinSigma.style.setProperty('display', 'none')
+        }, 100);
+      }, 300);
     })
 
     function randomPoin(targetIndex){
@@ -84,8 +96,8 @@ document.addEventListener('DOMContentLoaded', function(){
           let kemungkinan = Math.random();
           let poinAcak = Math.random();
           
-          if (kemungkinan < 0.75) {
-          randomValue = 30 + Math.pow(poinAcak, 2) * 70;
+          if (kemungkinan < 0.65) {
+          randomValue = 30 + Math.pow(poinAcak, 2) * 65;
           } else {
           randomValue = 17 + Math.sqrt(poinAcak) * 47;
           }
@@ -104,39 +116,47 @@ document.addEventListener('DOMContentLoaded', function(){
     function randomPersona() {
       let karakter = []
       if ( originalValuesPoin[0] <= 20) {
-        karakter.push('cringe kuadrat dikali jamet')
+        karakter.push('cringe kuadrat dikali jamet👺')
       } else if (originalValuesPoin[0] >= 20 && originalValuesPoin[0] <= 30) {
-        karakter.push('bocah pickme sok imut')
+        karakter.push('bocah pickme sok imut🥵')
       } else if (originalValuesPoin[0] >= 30 && originalValuesPoin[0] <= 50) {
         karakter.push('npc nolep')
       } else if (originalValuesPoin[0] >= 50 && originalValuesPoin[0] <= 60) {
         karakter.push('wibu bau bawang')
       } else if (originalValuesPoin[0] >= 60 && originalValuesPoin[0] <= 70) {
-        karakter.push('bocah dark system')
+        karakter.push('bocah dark system🤬')
       } else if (originalValuesPoin[0] >= 70 && originalValuesPoin[0] <= 80) {
-        karakter.push('rajin dan suka membantu orang')
+        karakter.push('rajin dan suka membantu orang🤗')
       } else if (originalValuesPoin[0] >= 80 && originalValuesPoin[0] <= 90) {
-        karakter.push('manipulatif dan bikin ketar ketir')
+        karakter.push('manipulatif dan bikin ketar ketir🥶')
       }else {
-        karakter.push('sangat dihormatin dan rispek')
+        karakter.push('sangat dihormatin dan rispek🤫')
       }
       
       if ( originalValuesPoin[4] <= 20) {
-        karakter.push('bocah akward')
+        karakter.push('bocah akward🤓')
+        gambarPersona.src = "gambar/p0020.jpg"
       } else if (originalValuesPoin[4] >= 20 && originalValuesPoin[4] <= 30) {
         karakter.push('rada freak')
+        gambarPersona.src = "gambar/p2030.jpg"
       } else if (originalValuesPoin[4] >= 30 && originalValuesPoin[4] <= 50) {
         karakter.push('normal')
+        gambarPersona.src = "gambar/p3050.jpg"
       } else if (originalValuesPoin[4] >= 50 && originalValuesPoin[4] <= 60) {
-        karakter.push('rapih dan wangi tapi umur 2 bulan thoriq udah haji')
+        karakter.push('rapih dan wangi tapi umur 2 bulan thoriq udah haji😌')
+        gambarPersona.src = "gambar/p5060.jpeg"
       } else if (originalValuesPoin[4] >= 60 && originalValuesPoin[4] <= 70) {
-        karakter.push('berwibawa')
+        karakter.push('remaja generasi emas 2040😇')
+        gambarPersona.src = "gambar/p6070.jpg"
       } else if (originalValuesPoin[4] >= 70 && originalValuesPoin[4] <= 80) {
-        karakter.push('karismatik')
+        karakter.push('karismatik😎')
+        gambarPersona.src = "gambar/p7080.jpg"
       } else if (originalValuesPoin[4] >= 80 && originalValuesPoin[4] <= 90) {
-        karakter.push('remaja generasi emas 2040')
+        karakter.push('bocil miliarder kripto🤑')
+        gambarPersona.src = "gambar/p8090.jpeg"
       }else {
-        karakter.push('the next presiden 2028')
+        karakter.push('the next presiden 2028😈')
+        gambarPersona.src = "gambar/p90100.jpg"
       }
       kepriBadianAsli = karakter.join(',')
       return karakter
